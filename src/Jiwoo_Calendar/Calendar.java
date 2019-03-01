4cdd70ce5d2aacc5d5806a636b2d364acd22607a
simplelife5757 <42859941+simplelife5757@users.noreply.github.com>
2019-03-01 18:28:08 +0900

    Update calendar main()

--- src/Jiwoo_Calendar/Calendar.java
+++ src/Jiwoo_Calendar/Calendar.java
@@ -3,7 +3,9 @@ package Jiwoo_Calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-			System.out.println("Hello, Calendar");
+			System.out.println("일 월 화 수 목 금 토");
+			System.out.println("------------");
+			System.out.println("1 2 3 4 5 6 7");
 	}
 
 }
