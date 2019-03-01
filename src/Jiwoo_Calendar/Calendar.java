9bb24771096f92cb147bdda759a4653bef5c9458
simplelife5757 <42859941+simplelife5757@users.noreply.github.com>
2019-03-01 21:57:49 +0900

    Update calculate maximum days of month

--- src/Jiwoo_Calendar/Calendar.java
+++ src/Jiwoo_Calendar/Calendar.java
@@ -1,7 +1,15 @@
 package Jiwoo_Calendar;
+import java.util.Scanner;
 
 public class Calendar {
 
+	private static final int[] MAX_DAYS	= {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month -1];
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("월  화  수  목  금  토  일");
 		System.out.println("------------");
@@ -9,6 +17,14 @@ public class Calendar {
 		System.out.println("8 9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("21 22 23 24 25 26 27");
+		
+		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		System.out.println("달을 입력하세요");
+		Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+		Calendar cal = new Calendar();
+		System.out.printf("%d월을 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		scanner.close();
 	}
 
 }
