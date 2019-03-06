6050a838eb7f080cf6bb8d83b901a3c89cac3024
simplelife5757 <42859941+simplelife5757@users.noreply.github.com>
2019-03-06 14:46:11 +0900

    Add number of repeat

--- src/Jiwoo_Calendar/Calendar.java
+++ src/Jiwoo_Calendar/Calendar.java
@@ -19,12 +19,30 @@ public class Calendar {
 		System.out.println("21 22 23 24 25 26 27");
 		
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		System.out.println("달을 입력하세요");
+//		System.out.println("달을 입력하세요");
+//		Scanner scanner = new Scanner(System.in);
+//		int month = scanner.nextInt();
+//		Calendar cal = new Calendar();
+//		System.out.printf("%d월을 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+//		scanner.close();
+//		
+		//반복 횟수에 따른  출력
+		String PROMPT ="cal>";
 		Scanner scanner = new Scanner(System.in);
-		int month = scanner.nextInt();
 		Calendar cal = new Calendar();
-		System.out.printf("%d월을 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		System.out.println("반복횟수를 입력하세요");
+		int numberOfInput = scanner.nextInt();
+		int[] months = new int[numberOfInput];
+		for (int i=0; i < numberOfInput; i++) {
+			System.out.println("달을 입력하세요");
+			System.out.print(PROMPT);
+			months[i] = scanner.nextInt();
+			}
+		for (int i=0; i < numberOfInput; i++) {
+			System.out.println(cal.getMaxDaysOfMonth(months[i]));
+		}
 		scanner.close();
+		
 	}
 
 }
